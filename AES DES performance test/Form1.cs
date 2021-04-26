4d33bc2a352a1d84a13e04b4a3733ff02d25d4a0
SANE <83189731+white-collor@users.noreply.github.com>
2021-04-26 13:22:29 +0500

    Add files via upload

---
+++ AES DES performance test/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+using System.Security.Cryptography;
+using System.IO;
+
+namespace AES_DES_performance_test
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+        static byte[] Encrypt(string plainText, byte[] Key, byte[] IV)
+        {
+            byte[] encrypted;
+            // Create a new AesManaged.    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create encryptor    
+                ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
+                // Create MemoryStream    
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    // Create crypto stream using the CryptoStream class. This class is the key to encryption    
+                    // and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream    
+                    // to encrypt    
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                    {
+                        // Create StreamWriter and write data to a stream    
+                        using (StreamWriter sw = new StreamWriter(cs))
+                            sw.Write(plainText);
+                        encrypted = ms.ToArray();
+                    }
+                }
+            }
+            // Return encrypted data    
+            return encrypted;
+        }
+        public string EncryptDataDES(string PlainText)
+        {
+            string _securityKey = "12345678";
+            //Getting the bytes of Input String.
+            byte[] toEncryptedArray = UTF8Encoding.UTF8.GetBytes(PlainText);
+
+            MD5CryptoServiceProvider objMD5CryptoService = new MD5CryptoServiceProvider();
+
+            //Gettting the bytes from the Security Key and Passing it to compute the Corresponding Hash Value.
+            byte[] securityKeyArray = objMD5CryptoService.ComputeHash(UTF8Encoding.UTF8.GetBytes(_securityKey));
+
+            //De-allocatinng the memory after doing the Job.
+            objMD5CryptoService.Clear();
+
+            var objTripleDESCryptoService = new TripleDESCryptoServiceProvider();
+
+            //Assigning the Security key to the TripleDES Service Provider.
+            objTripleDESCryptoService.Key = securityKeyArray;
+
+            //Mode of the Crypto service is Electronic Code Book.
+            objTripleDESCryptoService.Mode = CipherMode.ECB;
+
+            //Padding Mode is PKCS7 if there is any extra byte is added.
+            objTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var objCrytpoTransform = objTripleDESCryptoService.CreateEncryptor();
+
+            //Transform the bytes array to resultArray
+            byte[] resultArray = objCrytpoTransform.TransformFinalBlock(toEncryptedArray, 0, toEncryptedArray.Length);
+
+            //Releasing the Memory Occupied by TripleDES Service Provider for Encryption.
+            objTripleDESCryptoService.Clear();
+
+            //Convert and return the encrypted data/byte into string format.
+            return Convert.ToBase64String(resultArray, 0, resultArray.Length);
+        }
+        static string Decrypt(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            string plaintext = null;
+            // Create AesManaged    
+            using (AesManaged aes = new AesManaged())
+            {
+                // Create a decryptor    
+                ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
+                // Create the streams used for decryption.    
+                using (MemoryStream ms = new MemoryStream(cipherText))
+                {
+                    // Create crypto stream    
+                    using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                    {
+                        // Read crypto stream    
+                        using (StreamReader reader = new StreamReader(cs))
+                            plaintext = reader.ReadToEnd();
+                    }
+                }
+            }
+            return plaintext;
+        }
+        public string Tripledes(string TextToEncrypt)
+        {
+           string mysecurityKey = "1234567812345678";
+        byte[] MyEncryptedArray = UTF8Encoding.UTF8
+               .GetBytes(TextToEncrypt);
+
+            MD5CryptoServiceProvider MyMD5CryptoService = new
+               MD5CryptoServiceProvider();
+
+            byte[] MysecurityKeyArray = MyMD5CryptoService.ComputeHash
+               (UTF8Encoding.UTF8.GetBytes(mysecurityKey));
+
+            MyMD5CryptoService.Clear();
+
+            var MyTripleDESCryptoService = new
+               TripleDESCryptoServiceProvider();
+
+            MyTripleDESCryptoService.Key = MysecurityKeyArray;
+
+            MyTripleDESCryptoService.Mode = CipherMode.ECB;
+
+            MyTripleDESCryptoService.Padding = PaddingMode.PKCS7;
+
+            var MyCrytpoTransform = MyTripleDESCryptoService
+               .CreateEncryptor();
+
+            byte[] MyresultArray = MyCrytpoTransform
+               .TransformFinalBlock(MyEncryptedArray, 0,
+               MyEncryptedArray.Length);
+
+            MyTripleDESCryptoService.Clear();
+
+            return Convert.ToBase64String(MyresultArray, 0,
+               MyresultArray.Length);
+        }
+        public void EncryptAesManaged(string raw)
+        {
+            try
+            {
+                // Create Aes that generates a new key and initialization vector (IV).    
+                // Same key must be used in encryption and decryption    
+                using (AesManaged aes = new AesManaged())
+                {
+
+                    string securityKey = "1234567812345678";
+                    byte[] k= Encoding.ASCII.GetBytes(securityKey);
+                    // Encrypt string    
+                    byte[] encrypted = Encrypt(raw, k, aes.IV);
+                    // Print encrypted string    
+                    // Console.WriteLine($ "Encrypted data: {System.Text.Encoding.UTF8.GetString(encrypted)}");
+                    // Decrypt the bytes to a string.    
+                    string text = System.Text.Encoding.UTF8.GetString(encrypted);
+                    textBox2.Text = text;
+                    //string decrypted = Decrypt(encrypted, aes.Key, aes.IV);
+                    // Print decrypted string. It should be same as raw data    
+                   // Console.WriteLine($ "Decrypted data: {decrypted}");
+                }
+            }
+            catch (Exception exp)
+            {
+                textBox2.Text = exp.Message ;
+
+                //Console.WriteLine(exp.Message);
+            }
+           // Console.ReadKey();
+        }
+       
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string data = textBox1.Text;
+            string data2 = textBox4.Text;
+            string data3 = textBox6.Text;
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            EncryptAesManaged(data);
+            sw.Stop();
+            string aestime= sw.Elapsed.ToString();
+
+            string dataa = EncryptDataDES(data2);
+            textBox3.Text = dataa;
+            Stopwatch sw2 = new Stopwatch();
+            sw2.Start();
+            EncryptDataDES(data2);
+            sw2.Stop();
+            string destime = sw2.Elapsed.ToString();
+
+            string dataaa = Tripledes(data3);
+            textBox5.Text = dataaa;
+            Stopwatch sw3 = new Stopwatch();
+            sw3.Start();
+            Tripledes(data3);
+            sw3.Stop();
+            string tdestime = sw3.Elapsed.ToString();
+            this.dataGridView1.Rows.Add(aestime, destime, tdestime);
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.FilterIndex = 0;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFileName = openFileDialog1.FileName;
+                
+                byte[] m = StreamFile(selectedFileName);
+                string r=Encoding.UTF8.GetString(m);
+                textBox1.Text=r;
+                textBox4.Text = r;
+                 textBox6.Text = r;
+                Stopwatch sw = new Stopwatch();
+                sw.Start();
+                EncryptAesManaged(r);
+                sw.Stop();
+                string aestime = sw.Elapsed.ToString();
+
+                string dataa = EncryptDataDES(r);
+                textBox3.Text = dataa;
+                Stopwatch sw2 = new Stopwatch();
+                sw2.Start();
+                EncryptDataDES(m.ToString());
+                sw2.Stop();
+                string destime = sw2.Elapsed.ToString();
+
+                string dataaa = Tripledes(r);
+                textBox5.Text = dataaa;
+                Stopwatch sw3 = new Stopwatch();
+                sw3.Start();
+                Tripledes(r);
+                sw3.Stop();
+                string tdestime = sw3.Elapsed.ToString();
+                this.dataGridView1.Rows.Add(aestime, destime, tdestime);
+
+                //...
+            }
+        }
+        private byte[] StreamFile(string filename)
+        {
+            FileStream fs = new FileStream(filename, FileMode.Open, FileAccess.Read);
+
+            // Create a byte array of file stream length
+            byte[] ImageData = new byte[fs.Length];
+
+            //Read block of bytes from stream into the byte array
+            fs.Read(ImageData, 0, System.Convert.ToInt32(fs.Length));
+
+            //Close the File Stream
+            fs.Close();
+            return ImageData; //return the byte data
+        }
+    }
+}
